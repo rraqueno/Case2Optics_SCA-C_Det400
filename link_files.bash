0c6988c05cb363bb9bbe3108cf4dcc36c49d85e1
U-homestar\rvrpci <rvrpci@homestar.(none)>
2014-06-23 23:10:52 -0400

    Based on Case2Lunar_SCA-C_Det400

---
+++ link_files.bash
@@ -0,0 +1,6 @@
+cp -uv MODIS/A2014-104-2110_LAC_SST_geoL8TOA_B11 MODIS.img
+cp -uv MODIS/A2014-104-2110_LAC_SST_geoL8TOA_B11.hdr MODIS.img.hdr
+cp -uv ImageRadLatsLons_Column_1521_SCA-C_Det400_case2_104.img L8.img
+cp -uv ImageRadLatsLons_Column_1521_SCA-C_Det400_case2_104.hdr L8.hdr
+cp -uv ImageRadLatsLons_Column_1521_SCA-C_Det400_case2_104.bll L8.bll
+cp -uv B11_mar11_screened_USE_THIS_ONE_CSV.csv B11.csv
